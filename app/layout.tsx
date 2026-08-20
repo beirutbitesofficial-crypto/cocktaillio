faf261032a50f6faf25a6613128731eff86b7c84
beirutbitesofficial-crypto <beirut.bites.official@gmail.com>
2026-08-20 16:31:19 -0700

    Load polished POS styling globally

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata, Viewport } from "next";
 import "./globals.css";
 import "./mobile-nav.css";
 import "./brand-green.css";
+import "./service-polish.css";
 import AppRouteBridge from "./app-route-bridge";
 
 export const metadata: Metadata = {
